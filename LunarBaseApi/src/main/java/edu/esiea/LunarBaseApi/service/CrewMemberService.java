4ed1053f52c856c1a4d38c4eefc88dc9fb850528
Erwang64 <156552946+erwang64@users.noreply.github.com>
2026-05-22 15:03:49 +0200

    Ajout de la logique metier maximum capacity

--- LunarBaseApi/src/main/java/edu/esiea/LunarBaseApi/service/CrewMemberService.java
+++ LunarBaseApi/src/main/java/edu/esiea/LunarBaseApi/service/CrewMemberService.java
@@ -19,22 +19,26 @@ public class CrewMemberService {
         this.lunarBaseRepo = lunarBaseRepo;
     }
 
-    public CrewMember addCrewMember(CrewMember newMember, Integer lunarBaseId) throws ServiceException {
+public CrewMember addCrewMember(CrewMember newMember, Integer lunarBaseId) throws ServiceException {
         
-
         if (lunarBaseId != null && lunarBaseId > 0) {
-            
-
+          
             LunarBase base = lunarBaseRepo.findById(lunarBaseId)
-                    .orElseThrow(() -> new ServiceException("Impossible d'affecter : La basen'existe pas."));
-            base.getCrewMembers().add(newMember);
+                    .orElseThrow(() -> new ServiceException("Impossible d'affecter : La basen'existe pas"));
+            int currentCrewSize = 0;
             
+            if (base.getCrewMembers() != null) {
+                currentCrewSize = base.getCrewMembers().size();
+            }
+            
+            if (currentCrewSize >= base.getMaximalCapacity()) {
+                throw new ServiceException("Affectation impossible : La base a atteint sa capacité maximale membres");
+            }
+            base.getCrewMembers().add(newMember);
             lunarBaseRepo.save(base);
             
             return newMember;
         }
-        
-
         return crewMemberRepo.save(newMember);
     }
     
